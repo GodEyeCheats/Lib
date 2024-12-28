4feca0e4990cebeecd140367928273d44a774416
GodEyeCheats <jitendrasinghbaghel035@gmail.com>
2024-12-29 04:04:52 +0530

    Update DownloadJSON_TESTING.php

--- DownloadJSON_TESTING.php
+++ DownloadJSON_TESTING.php
@@ -1,5 +1,4 @@
 {
   "version": "0.09",
-  "link": "https://raw.githubusercontent.com/GodEyeCheats/Lib/main/libGVoicePlugin.so",
-  "link1": "https://raw.githubusercontent.com/GodEyeCheats/Lib/main/libGVoicePlugin1.so"
+  "link": "https://raw.githubusercontent.com/GodEyeCheats/Lib/main/libGVoicePlugin.so"
 }
