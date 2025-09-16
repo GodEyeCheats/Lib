a1948449ee31f35e2943144b4d658205858004a2
GodEyeCheats <jitendrasinghbaghel035@gmail.com>
2025-09-16 19:33:52 +0530

    Update DownloadJSON_TESTING.php

--- DownloadJSON_TESTING.php
+++ DownloadJSON_TESTING.php
@@ -4,6 +4,11 @@
       "name": "libGVoicePlugin.so",
       "url": "https://raw.githubusercontent.com/GodEyeCheats/Lib/main/libGVoicePlugin.so",
       "version": 1.0
+    },
+    {
+      "name": "libpubgm.so",
+      "url": "https://raw.githubusercontent.com/GodEyeCheats/Lib/main/libpubgm.so",
+      "version": 1.0
     }
   ]
 }
